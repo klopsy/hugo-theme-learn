c29add82525583d1742ef82ee1a0aae74d142df7
klops <signup@mrklop.com>
2019-09-15 08:52:34 -0700

    missing js to hide abcjs editor

---
+++ static/js/custom.js
@@ -0,0 +1,11 @@
+window.toggleDiv = function (divId) {
+    var x = document.getElementById(divId);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+
+    } else {
+        x.style.display = "none";
+
+    }
+
+}
